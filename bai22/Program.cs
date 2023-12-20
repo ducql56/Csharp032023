a4f8a79ff6ca8720c1cf6807c010a4524235a2f6
ducql56 <longduc91193@gmail.com>
2023-12-20 23:01:49 +0700

    lession3

---
+++ bai22/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace bai22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("moi nhap vao so n 1-99:");
+            n = int.Parse(Console.ReadLine());
+            while (n < 1 || n > 99)
+            {
+                Console.WriteLine("nhap lai n,n chi nhan 1-99");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(" ban da nhap xong gia tri cua n:{0}", n);
+            Console.ReadKey();
+        }
+    }
+}
