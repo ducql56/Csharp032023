a1028bd6a3a5d4c756d532d55f6f4d4f88f62116
ducql56 <longduc91193@gmail.com>
2023-12-20 22:50:13 +0700

    creat lesion 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Wo66rld!");
+Console.WriteLine("Hello,44 World!");
+Console.WriteLine("Hello, Worjjld!");
+Console.WriteLine("Hello, Worlllld!");
+Console.WriteLine("Hello, 66World!");
+Console.WriteLine("Hello, World!");
+
+
+Console.ReadKey();
